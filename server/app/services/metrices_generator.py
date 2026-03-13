cdf112a2ba42a3e31451f0bbc99c352a24004144
Daniel Enesi <enesidaniel.120064@gmail.com>
2026-03-13 02:36:06 -0500

    Add metrics documentation in metrices_generator.py and update requirements.txt

--- server/app/services/metrices_generator.py
+++ server/app/services/metrices_generator.py
@@ -1,2 +1,23 @@
+"""
+Metrics to get from what the database give
 
+ Brand mention per query - against competitors - every 100 queries.
+    - Divide across LLMS - Gpt x%, Gemini y%
 
+- Answers citing company website per total mention
+    E.g. Dell. How many were mentioned by chat were from the company website.
+    - Which other domains do the AI get info about the product
+
+- Ranking among competitors  - DONE
+    - mentions and for links per query (...)
+    - Top Recommendation Rate
+    - Ranking
+
+- Factual score - DONE
+    - Links Based on AI output about A Link/product vs actual content. if it's related, if it contains the valid info
+
+- Errors - DONE
+    Link route | error code/type
+
+Output these metrics based on models in the db. Get the insights from thoughs models
+"""
